0a200dd2aeb46741eb6aca17390dc8f5001aa70b
bymac-si <mcastroabarca@gmail.com>
2021-11-04 20:46:12 -0300

    agregando validaciones

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,7 @@
 class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :product
+
+  validates :product_id, presence: true
+  validates :quantity, presence: true, numericality:true
 end
