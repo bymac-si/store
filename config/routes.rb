f893b19b7cc4d53671255c8d9493fd6735fb6b22
bymac-si <mcastroabarca@gmail.com>
2021-11-03 22:36:44 -0300

    primer CRUD, Category

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'categories/create'
+  get 'categories/edit'
+  get 'categories/list'
+  get 'categories/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
