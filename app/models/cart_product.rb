ef60af8da409d07e91fc4a092e3716a1d753f62f
bymac-si <mcastroabarca@gmail.com>
2021-11-16 20:47:02 -0300

    agragando correcciones

--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -5,5 +5,16 @@ class CartProduct < ApplicationRecord
   validates :cart_id, presence: true
   validates :product_id, presence: true
   validates :quantity, presence: true, numericality: true
+
+  private
+  def calculate_total
+    total = 0
+    self.cart.cart_products.each do |cp| 
+      total += cp.quantity * cp.product.price
+    end
+    self.cart.total = total
+    self.cart.save
+    
+  end
   
 end
