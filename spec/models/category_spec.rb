15c9ead9a1a2d242ed994717c065f011bbcc74dd
bymac-si <mcastroabarca@gmail.com>
2021-11-17 21:02:26 -0300

    agrgando base para pruebas

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+
+  describe '#create' do
+    it 'when attributes are not valid' do
+      #Arrange 
+      #Act 
+      #Assert
+    end
+end
+
+end
